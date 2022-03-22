205e5fc02bd4691a2db4f026552e53826e2df8ee
Arkel Tatiana <tata300-1@yandex.ru>
2022-03-22 11:43:29 +0700

    Проектная работа4 вариант2

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 * В дальнейшем планируется реализация технологии добавления и удаления фотографий.
 
 
-* [Ссылка на проект в GIThub](https://github.com/arkel-tatiana/mesto.git)
+* [Ссылка на проект](https://github.com/arkel-tatiana/mesto.git)
 
 
--- scripts/index.js
+++ scripts/index.js
@@ -19,22 +19,22 @@ function closePopup() {
 openButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 
-let name1; 
-let info1;
+//let nameuser; 
+//let infouser;
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
 //    let nameInput = formElement.querySelector('.popup__input_name_username');
 //	let jobInput = formElement.querySelector('.popup__input_name_userjob');
-    name1 = nameInput.value;
-    info1 = jobInput.value;
+//    name1 = nameInput.value;
+//    info1 = jobInput.value;
 //    console.log(name1 + info1)
 //    let title = document.querySelector('.profile__title');
 //    let subtitle = document.querySelector('.profile__subtitle');
 //    console.log(title)
-    title.textContent = name1;
-    subtitle.textContent = info1;
-    
+    title.textContent = nameInput.value;
+    subtitle.textContent = jobInput.value;
+    closePopup()
     
 }
 formElement.addEventListener('submit', formSubmitHandler);
